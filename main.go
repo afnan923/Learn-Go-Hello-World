2ea3f3b961b5007ba0f2737c30d1cc75eeda96f1
afnan923 <susumurni013@gmail.com>
2026-01-22 01:53:25 +0700

    feat:variabel and deklarasi

--- main.go
+++ main.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main(){
 	fmt.Println("Hello,World!")
+
+	//deklarasi variabel dengan tipe data
+	var name = "Afnan"
+	var umur = 12
+	//deklarasi dengan tipe inferensi atau untuk koma
+	version := 1.23
+	//deklarasi konstanta
+	const pi = 3.14
+	
+	//panggil semua variabel atau deklarasi
+	fmt.Println("Nama",name)
+	fmt.Println("Umur",umur)
+	fmt.Println("Versi",version)
+	fmt.Println("Nilai Pi",pi)
 }
